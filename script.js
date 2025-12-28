f320592f5872b4e42ca8c8b88eac17665fe81e89
Wafiq Abdur Rozaq <159865339+wafiq8@users.noreply.github.com>
2025-12-28 21:29:45 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 document.querySelector('.cta-button').addEventListener('click', function () {
     alert('Terima kasih! Kami akan segera melayani Anda 🙏');
 });
+const reveals = document.querySelectorAll(".reveal");
+
+const revealOnScroll = () => {
+    const windowHeight = window.innerHeight;
+    const revealPoint = 120;
+
+    reveals.forEach(el => {
+        const revealTop = el.getBoundingClientRect().top;
+
+        if (revealTop < windowHeight - revealPoint) {
+            el.classList.add("active");
+        }
+    });
+};
+
+window.addEventListener("scroll", revealOnScroll);
+window.addEventListener("load", revealOnScroll);
